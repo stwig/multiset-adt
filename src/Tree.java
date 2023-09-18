6218e9f5bef497cbd359c1b9ad2a9381897a6562
HC123abc <126921906+HC123abc@users.noreply.github.com>
2023-09-18 15:13:04 -0400

    first few methods

--- src/Tree.java
+++ src/Tree.java
@@ -1,5 +1,35 @@
+import java.util.ArrayList;
 
 public class Tree {
     private Object root;
-    private Object;
+    private  ArrayList<Tree> subtree;
+
+    public Tree( Object root, ArrayList<Tree> subtree){
+        this.root = root;
+        if (subtree == null){
+            this.subtree = new Arraylist<Tree>();
+        }
+        else {
+            this.subtree = subtree;
+        }
+    }
+
+    public boolean isEmpty(){
+        return this.root == null;
+    }
+
+    public int len(){
+        if (this.isEmpty()){
+            return 0;
+        }
+        else{
+            int size = 1;
+            for(Tree i : this.subtree){
+                size += i.len();
+            }
+            return size;
+        }
+    }
+
+
 }
